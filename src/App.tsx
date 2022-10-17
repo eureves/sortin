a3cb1aeee9baca99ab01c6b5bd5fc00c35270287
eureves <eureves@proton.me>
2022-10-17 16:21:26 +0300

    added auto sort

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,56 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo as useEffect, useCallback } from "react";
 import ButtonsBlock from "./components/ButtonsBlock/ButtonsBlock";
 import SortingView from "./components/SortingView/SortingView";
 
+const INITIAL_ARRAY = [7, 5, 4, 11, 2, 1];
+
+const swapArray = (array: number[], index: number): number[] => {
+  const tempArr = [...array];
+  const temp = tempArr[index + 1];
+  tempArr[index + 1] = tempArr[index];
+  tempArr[index] = temp;
+  return tempArr;
+};
+
+const bubbleSort = (array: number[], step: number) => {
+  if (array[step] > array[step + 1]) return swapArray(array, step);
+  return array;
+};
+
 function App() {
-  const [array, setArray] = useState<number[]>([7, 5, 4, 11, 2]);
+  const [array, setArray] = useState<number[]>(INITIAL_ARRAY);
   const [step, setStep] = useState<number>(0);
   const [start, setStart] = useState<boolean>(false);
+  const [intervalId, setIntervalId] = useState<number>();
 
-  const bubbleSortManual = (array: number[], step: number) => {
-    let i: number = 0;
-
-    if (step + 2 >= array.length) {
+  useEffect(() => {
+    console.log(step);
+    if (step + 2 <= array.length) {
+      setArray((state) => bubbleSort(state, step));
+    } else {
       setStep(0);
-      i++;
     }
+  }, [step]);
 
-    if (array[step] > array[step + 1]) return swapArray(array, step);
-    return array;
-  };
+  // const interval = useCallback(() => {
+  //   return;
+  // }, []);
 
-  const swapArray = (array: number[], index: number): number[] => {
-    const tempArr = [...array];
-    const temp = tempArr[index + 1];
-    tempArr[index + 1] = tempArr[index];
-    tempArr[index] = temp;
-    return tempArr;
-  };
+  useEffect(() => {
+    if (start) {
+      setIntervalId(setInterval(() => setStep((state) => state + 1), 1000));
+    } else {
+      clearInterval(intervalId);
+    }
+    return clearInterval(intervalId);
+  }, [start]);
 
   const handleStart = () => {
+    console.log("start");
     setStart((start) => !start);
   };
 
   const handleStepChange = () => {
-    setArray((state) => bubbleSortManual(state, step));
     setStep((step) => step + 1);
   };
 
