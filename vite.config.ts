117f66b073468e608f206803d842836373ad0043
eureves <eureves@proton.me>
2022-10-20 14:33:39 +0300

    gh pages base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "eureves.github.io/sortin/",
+  base: "/eureves.github.io/sortin/",
   plugins: [react()],
 });
